314cbc51577c8635fe976e59ad15d10a71834c4d
KuldeepJha5176 <ranacjha@gmail.com>
2024-11-20 02:31:00 +0530

    signin end point completed with token and hashed password

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import mongoose from "mongoose";
-import { Jwt } from "jsonwebtoken";
-import bcrypt from 'bcrypt';
+import jwt from "jsonwebtoken";
+import bcrypt, { hash } from "bcrypt";
 import { UserModel } from "./db";
+import { ExitStatus } from "typescript";
+import { JWT_PASSWORD } from "./config";
 
 const app = express();
 app.use(express.json());
@@ -26,7 +28,39 @@ app.post("/api/v1/signup", async (req, res) => {
     });
   }
 });
-app.post("/api/v1/signin", (req, res) => {});
+app.post("/api/v1/signin", async (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  const existingUser = await UserModel.findOne({
+    username,
+    
+  });
+
+  if (existingUser && existingUser.password) {
+    const passwordMatch = await bcrypt.compare(password, existingUser.password);
+    if (passwordMatch) {
+      const token = jwt.sign(
+        {
+          id: existingUser._id,
+        },
+        JWT_PASSWORD
+      );
+
+      res.json({
+        token,
+      });
+    } else {
+      res.status(403).json({
+        message: "Incorrrect credentials",
+      });
+    }
+  } else {
+    res.status(403).json({
+      message: "User not found",
+    });
+  }
+});
+
 app.post("/api/v1/content", (req, res) => {});
 app.get("/api/v1/content", (req, res) => {});
 app.delete("/api/v1/content", (req, res) => {});
